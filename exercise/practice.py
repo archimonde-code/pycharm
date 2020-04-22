81e100375c6df59ece483053020175fd95e7b9c5
Archimonde <gilbert_v@yeah.net>
2020-04-22 21:11:19 +0800

    add selection sort

--- exercise/practice.py
+++ exercise/practice.py
@@ -1,6 +1,8 @@
+import time
+
 """
 主要练习的是Python的排序算法，
-主要包含冒泡排序、选择排序
+主要包含冒泡排序、选择排序、
 """
 
 
@@ -19,6 +21,25 @@ class SortFunction:
                     list_sort[j], list_sort[j + 1] = list_sort[j + 1], list_sort[j]
         return list_sort
 
+    # 选择排序
+    def selection_sort(self):
+        sort_list = self
+        for i in range(len(sort_list) - 1):
+            # 记录最小数的索引
+            min_index = i
+            for j in range(i + 1, len(sort_list)):
+                if sort_list[j] < sort_list[min_index]:
+                    min_index = j
+                # i 不是最小数时，将 i 和最小数进行交换
+                if i != min_index:
+                    sort_list[i], sort_list[min_index] = sort_list[min_index], sort_list[i]
+        return sort_list
+
 
 array_list = [32, 54, 12, 90, 65, 34, 27, 1, 87, 74, 15]
+
+# 对数组array_list进行冒泡排序，并输出排序后的数组
 print(SortFunction.bubble_sort(array_list))
+
+# 对数组array_list进行选择排序，并输出排序后的数组
+print(SortFunction.selection_sort(array_list))
